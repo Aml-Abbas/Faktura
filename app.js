d09f4d39f637ce8282f0fcfec9cb87e3e235541c
Aml Abbas <aml.abbas@hotmail.com>
2018-02-15 11:52:55 +0100

    java ändring

--- app.js
+++ app.js
@@ -11,19 +11,6 @@ function time3 (){
     n =  new Date();
     y = n.getFullYear();
     m = n.getMonth() +2 ;
-    d = n.getDate();
+    d = n.getDate() -1;
     document.getElementById("demo3").innerHTML = m + "/" + d + "/" + y;
 }
-
-var item1=["Stol",24,250];
-var item2=["Skrivbord",2,750];
-var item0=["Gåva vid köp",1,0];
- function MyFunktion () {
-     item1.push(item1[1]*item1[2]);
-     item2.push(item2[1]*item2[2]);
-     item0.push(item0[2]);
- }
-function varor() {
-    document.getElementById("1") .innerHTML= item1[0];
-}
-
